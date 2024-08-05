27cf01d3436d82bc35bfcbb3274b6e30adc0ef5a
Catalin Prodan <catalin.prodan@newpixel.ro>
2024-08-05 12:56:21 +0300

    wp

--- routes/console.php
+++ routes/console.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 // Artisan::command('inspire', function () {
 //     $this->comment(Inspiring::quote());
 // })->purpose('Display an inspiring quote')->hourly();
 
-Artisan::command('backup:clean')->daily()->at('04:00');
-Artisan::command('backup:run')->daily()->at('05:00');
+Schedule::command('backup:clean')->daily()->at('01:00');
+Schedule::command('backup:run')->daily()->at('01:30');
